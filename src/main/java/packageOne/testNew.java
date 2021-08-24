7d91aea868dc21aaf824cb18fcc7d356ba4879ea
fluorita <hannshum@gmail.com>
2021-08-24 17:45:56 +0300

    hannaCommit
    new commit

--- src/main/java/packageOne/testNew.java
+++ src/main/java/packageOne/testNew.java
@@ -3,7 +3,7 @@ package packageOne;
 public class testNew {
 
     public static void main(String[] args) {
-        System.out.println("Hello WorldmodifiedHanna!");
+        System.out.println("Hello WTestNew");
     }
 }
 
