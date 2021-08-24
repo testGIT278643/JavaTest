ea8e17cd6cc9d577231c922843fdad5d8bb61b28
fluorita <hannshum@gmail.com>
2021-08-24 17:47:41 +0300

    TestCommit
    TestCommit

--- src/main/java/packageOne/testNew.java
+++ src/main/java/packageOne/testNew.java
@@ -3,7 +3,7 @@ package packageOne;
 public class testNew {
 
     public static void main(String[] args) {
-        System.out.println("Hello WTestNew");
+        System.out.println("Hello WTestNewmodified");
     }
 }
 
