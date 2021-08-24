5c055ce2fd529c4d6b956a424bbcfc6a5bfe5fbb
fluorita <hannshum@gmail.com>
2021-08-24 17:37:09 +0300

    Update TestHanna.java

--- src/main/java/packageOne/TestHanna.java
+++ src/main/java/packageOne/TestHanna.java
@@ -2,7 +2,7 @@ package packageOne;
 
 public class TestHanna {
     public static void main(String[] args) {
-        System.out.println("Hello WorldmodifiedHanna!");
+        System.out.println("Hello WorldmodifiedHannanew!");
     }
 }
 
